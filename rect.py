bd10307d1e08f4636cd9a81dae877c9309e8ad45
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-30 09:55:19 +0100

    Add files via upload

--- rect.py
+++ rect.py
@@ -2,19 +2,19 @@ import tkinter as tk
 
 
 class myapps:
-    def __init__(self,root:tk.Tk):
+    def __init__(self,root:tk.Tk,titles:str,widths:int,heights:int,backgrounds:str,steps:int,colors:int):
         self.root=root
-        self.root.title("Rect")
-        self.root.geometry("640x480")
-        self.root.configure(background="black")
-        self.canvas=tk.Canvas(background="black",width=640,height=480)
+        self.root.title(titles)
+        self.root.geometry(str(widths)+"x"+str(heights))
+        self.root.configure(background=backgrounds)
+        self.canvas=tk.Canvas(background=backgrounds,width=widths,height=heights)
         self.canvas.pack(padx=1,pady=1)
         c=self.canvas
-        for a in range(0,640,40):
-            for b in range(0,480,20):
-                bb=(b//20 & 1)
+        for a in range(0,widths,steps*2):
+            for b in range(0,heights,steps):
+                bb=(b//steps & 1)
                 
-                c.create_rectangle(a+(bb*20),b,a+20+(20*bb),b+20,fill="white")
+                c.create_rectangle(a+(bb*steps),b,a+steps+(steps*bb),b+steps,fill=colors )
         
         
 
@@ -22,7 +22,15 @@ class myapps:
 
 
 
+def starts(titles:str,widths:int,heights:int,backgrounds:str,steps:int,colors:int):
+    root=tk.Tk()
+    apps=myapps(root,titles,widths,heights,backgrounds,steps,colors)
+    root.mainloop()
 
-root=tk.Tk()
-apps=myapps(root)
-root.mainloop()
+titles:str="fill grids"
+widths:int=640
+heights:int=480
+backgrounds:str="black"
+steps:int=20
+colors:int="white"
+starts(titles,widths,heights,backgrounds,steps,colors)
